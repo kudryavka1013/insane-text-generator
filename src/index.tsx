af6231678505190622a0e1f9805a594f2af44e16
kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2023-05-26 18:26:47 +0800

    chore: add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// @ts-ignore react18类型没跟上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
