78d0c87d59e926c50163ccd388164596774653db
Kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2023-06-01 19:36:05 +0800

    feat: copy button animation

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Card } from "./Card"
 import { Config } from "./Config"
+import { CheckIcon } from '@heroicons/react/20/solid'
 
 export const Editor = (props: any) => {
   const [input, setInput] = useState<string>('')
@@ -30,9 +31,13 @@ export const Editor = (props: any) => {
     setTextarea('')
   }
 
-  const onCopy = () => {
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(textarea);
+  const onCopy = async () => {
+    try {
+      if (navigator.clipboard) {
+        await navigator.clipboard.writeText(textarea);
+      }
+    } catch (e) {
+      console.error(e)
     }
   }
 
@@ -58,7 +63,12 @@ export const Editor = (props: any) => {
           rows={4}
           className="min-h-[6rem] w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out selection:bg-sky-900/75 selection:text-white"></textarea>
         <div className="flex-col shrink-0">
-          <button className="hover:bg-indigo-600 mb-4 block w-16 py-2 px-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none transition-colors duration-100 ease-in-out" onClick={onCopy}>复制</button>
+          <button className="relative group hover:bg-indigo-600 mb-4 block w-16 py-2 px-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none transition-colors duration-100 ease-in-out" onClick={onCopy}>
+            <div className="group-active:duration-0 group-active:delay-0 group-active:opacity-0 opacity-100 duration-500 delay-1000 ease-in" >
+              复制
+            </div>
+            <CheckIcon className="absolute m-auto w-8 left-0 right-0 top-0 bottom-0 group-active:duration-0 group-active:delay-0 group-active:opacity-100 opacity-0 duration-500 delay-1000 ease-in" />
+          </button>
           <button className="hover:bg-indigo-600 block w-16 py-2 px-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow transition-colors duration-100 ease-in-out focus:outline-none" onClick={onClear}>清空</button>
           {/* <button className="block">替换</button> */}
         </div>
