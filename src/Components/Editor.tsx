1abfabe1f094e7b03e5934669e0ceb7ca1a9e9df
Kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2023-06-01 16:58:32 +0800

    feat: button style

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -39,25 +39,27 @@ export const Editor = (props: any) => {
   return (
     <Card>
       <Config config={config} setConfig={setConfig} />
-      <div className="flex mt-8 mb-4">
+      <div className="flex mt-8 mb-4 gap-4">
         <input
           value={input}
           onChange={onInputChange}
           type="text"
           onKeyDown={onKeyUp}
+          autoFocus
           placeholder="输入文本，按下回车"
           className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out selection:bg-sky-900/75 selection:text-white" />
-        <button className="inline-block w-16" onClick={onEnter}>生成</button>
+        <button className="shrink-0 inline-block w-16 py-2 px-3 hover:bg-indigo-600 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none" onClick={onEnter}>生成</button>
       </div>
-      <div className="flex">
+      <div className="flex gap-4">
         <textarea
           value={textarea}
           onChange={onTextareaChange}
           placeholder="输出内容"
-          className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out selection:bg-sky-900/75 selection:text-white"></textarea>
-        <div className="flex-col">
-          <button className="block w-16" onClick={onCopy}>复制</button>
-          <button className="block w-16" onClick={onClear}>清空</button>
+          rows={4}
+          className="min-h-[6rem] w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out selection:bg-sky-900/75 selection:text-white"></textarea>
+        <div className="flex-col shrink-0">
+          <button className="hover:bg-indigo-600 mb-4 block w-16 py-2 px-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none" onClick={onCopy}>复制</button>
+          <button className="hover:bg-indigo-600 block w-16 py-2 px-3 bg-indigo-500 text-white text-sm font-semibold rounded-md shadow focus:outline-none" onClick={onClear}>清空</button>
           {/* <button className="block">替换</button> */}
         </div>
       </div>
