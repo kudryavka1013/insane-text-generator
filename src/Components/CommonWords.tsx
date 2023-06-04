a34636735b3ffc6458c043fddda11d8b2170c108
Kudryavka <949495534@qq.com>
2023-06-04 14:39:25 +0800

    feat: icon

--- src/Components/CommonWords.tsx
+++ src/Components/CommonWords.tsx
@@ -4,6 +4,7 @@ import { Card } from "./Card"
 export const CommonWords = () => {
   return (
     <Card title="常用">
+      <span className="text-indigo-500">没这功能啦</span>
     </Card>
   )
 }
--- src/Components/HistoryList.tsx
+++ src/Components/HistoryList.tsx
@@ -4,6 +4,7 @@ import { Card } from "./Card"
 export const HistoryList = () => {
   return (
     <Card title="历史">
+      <span className="text-indigo-500">没这功能啦</span>
     </Card>
   )
 }
